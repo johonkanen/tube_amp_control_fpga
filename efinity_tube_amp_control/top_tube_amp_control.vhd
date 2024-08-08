4b0ed92709c0d8b2b5eeea540f056ae42b1a5d9f
Jari Honkanen <jaarhonka@yahoo.com>
2024-08-08 20:26:07 +0300

    add initial test pwm

--- efinity_tube_amp_control/top_tube_amp_control.vhd
+++ efinity_tube_amp_control/top_tube_amp_control.vhd
@@ -12,14 +12,10 @@ entity top is
         uart_rx          : in std_logic;
         uart_tx          : out std_logic;
 
-        power_connector_io_0            : out std_logic;
-        power_connector_io_1            : out std_logic;
-        power_connector_io_2            : out std_logic;
-        power_connector_io_3            : out std_logic;
-        power_connector_io_15_downto_4  : out std_logic_vector(15 downto 4);
-        power_connector_io_16           : out std_logic;
-        power_connector_io_23_downto_17 : out std_logic_vector(23 downto 17);
-        gpio_inst28                     : out std_logic_vector(3 downto 0);
+        anode_gate_hi  : out std_logic;
+        anode_gate_lo  : out std_logic;
+        preamp_gate_hi : out std_logic;
+        preamp_gate_lo : out std_logic;
 
         -- iic adc
         adc_scl     : out std_logic;
@@ -51,91 +47,79 @@ architecture rtl of top is
 
     signal bus_out : fpga_interconnect_record := init_fpga_interconnect;
 
-    signal test_data : unsigned(15 downto 0) := (others => '0');
+    signal duty_ratio       : natural range 0 to 8191    := 63;
+    signal duty             : natural range 0 to 8191    := 63;
+    signal pwm_counter      : natural range 0 to 2**13   := 0;
 
-    signal power_connector_io1 : std_logic_vector(15 downto 0);
-    signal power_connector_io2 : std_logic_vector(15 downto 0);
+    signal anode_pwm : std_logic_vector(1 downto 0) := "00";
 
-    signal counter_for_10us : natural range 0 to 2**16-1 := 0;
-    signal duty_ratio :       natural range 0 to 8191 := 63;
-    signal pwm_counter :      natural range 0 to 1023 := 0;
+    signal deadtime_counter : natural range 0 to 255 := 160;
 
 begin
 
     adc_sda_OE  <= '0';
-    adc_sda_OUT <= power_connector_io1(0);
+    adc_sda_OUT <= '0';
     adc_scl     <= '1';
 
-    dac_sda_OE  <= '1';
-    dac_sda_OUT <= power_connector_io1(0);
-    dac_scl     <= power_connector_io1(0);
+    dac_sda_OE  <= '0';
+    dac_sda_OUT <= '1';
+    dac_scl     <= '1';
 
-    admux    <= (others => power_connector_io1(0));
+    admux    <= (others => '1');
     ad_cs    <= '1';
     -- ad_data  <= power_connector_io1(0);
     ad_clock <= '1';
 
-    power_connector_io_0            <= power_connector_io1(0);
-    power_connector_io_1            <= power_connector_io1(1);
-    power_connector_io_2            <= power_connector_io1(2);
-    power_connector_io_3            <= power_connector_io1(3);
-    power_connector_io_15_downto_4  <= power_connector_io1(15 downto 4);
-    power_connector_io_16           <= power_connector_io2(0);
-    power_connector_io_23_downto_17 <= power_connector_io2(7 downto 1);
-
 ------------------------------------------------
     board_test_main : process(main_clock)
-        ----
-        function "+"
-        (
-            left : std_logic_vector; right : integer
-        )
-        return std_logic_vector
-        is
-        begin
-            return std_logic_vector(unsigned(left) + right);
-        end "+";
-        ----
     begin
         if rising_edge(main_clock) then
 
             init_bus(bus_out);
             connect_read_only_data_to_address(bus_from_communications, bus_out, 1, 44252);
-            connect_data_to_address(bus_from_communications, bus_out, 10, power_connector_io1);
-            connect_data_to_address(bus_from_communications, bus_out, 11, power_connector_io2);
             connect_data_to_address(bus_from_communications, bus_out, 12, duty_ratio);
 
-            if data_is_requested_from_address(bus_from_communications, 2) then
-                test_data <= test_data + 1;
-                write_data_to_address(bus_out, 0, std_logic_vector(test_data));
-            end if;
-
-            if write_is_requested_to_address(bus_from_communications, 2) then
-                test_data <= to_unsigned(get_data(bus_from_communications),test_data'length);
-            end if;
-
-
-            if counter_for_10us < 1279 then               
-                counter_for_10us <= counter_for_10us + 1; 
-            else                                          
-                counter_for_10us <= 0;                    
-            end if;                                       
-
-            if counter_for_10us > 128 then              
-                power_connector_io1 <= (others => '0'); 
-                power_connector_io2 <= (others => '0'); 
-            else                                        
-                power_connector_io1 <= (others => '1'); 
-                power_connector_io2 <= (others => '1'); 
-            end if;                                     
-
-            if pwm_counter < 1023 then
+            if pwm_counter < 4999 then
                 pwm_counter <= pwm_counter + 1;
             else
                 pwm_counter <= 0;
             end if;
 
-            gpio_inst28 <= hrpwm(pwm_counter, duty_ratio);
+            if duty_ratio > integer(0.5*5000.0) then
+                duty <= integer(0.5*5000.0);
+            else
+                duty <= duty_ratio;
+            end if;
+
+            if duty < pwm_counter then
+                anode_pwm(0) <= '1';
+            else
+                anode_pwm(0) <= '0';
+            end if;
+            anode_pwm(1) <= anode_pwm(0);
+            if anode_pwm(1) /= anode_pwm(0) then
+                deadtime_counter <= 0;
+            end if;
+
+            if deadtime_counter < 159 then
+                anode_gate_hi  <= '0';
+                anode_gate_lo  <= '0';
+                preamp_gate_hi <= '0';
+                preamp_gate_lo <= '0';
+            else
+                anode_gate_hi  <= anode_pwm(1);
+                anode_gate_lo  <= not anode_pwm(1);
+
+                preamp_gate_hi <= anode_pwm(1);
+                preamp_gate_lo <= not anode_pwm(1);
+            end if;
+
+            if duty_ratio < 300 then
+                anode_gate_hi  <= '0';
+                anode_gate_lo  <= '0';
+                preamp_gate_hi <= '0';
+                preamp_gate_lo <= '0';
+            end if;
 
         end if;
     end process;
@@ -156,7 +140,6 @@ begin
             uart_rx                 => uart_rx               ,
             uart_tx                 => uart_tx               ,
             bus_to_communications   => bus_to_communications ,
-            bus_from_communications => bus_from_communications
-        );
+            bus_from_communications => bus_from_communications);
 
 end rtl;
