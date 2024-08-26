9d14552541e78d43d6e10872a03ea7b678d7e469
Jari Honkanen <jari.honkanen@danfoss.com>
2024-08-26 17:06:35 +0300

    add uart measurement test script to hw_test.py

--- test_hw.py
+++ test_hw.py
@@ -19,3 +19,46 @@ print("data from test address 2 : " + str(uart.request_data_from_address(2)))
 uart.write_data_to_address(3, 0)
 uart.plot_data_from_address(2, 50000)
 
+def plot_channel(channel_num):
+    print("0")
+    # time.sleep(1.1)
+    uart.write_data_to_address(3, channel_num)
+    print("1")
+    # time.sleep(0.1)
+    uart.request_data_stream_from_address(2,50000)
+    # time.sleep(1.1)
+    print("2")
+    time.sleep(0.1)
+    print("3")
+    uart.write_data_to_address(12,1000)
+    print("4")
+    # time.sleep(0.1)
+    print("5")
+    uart.write_data_to_address(12,4000)
+    print("6")
+    # time.sleep(0.1)
+    print("7")
+    data = uart.get_streamed_data(50000)
+    print("8")
+    print("9")
+    uart.write_data_to_address(12,1000)
+    print("10")
+    return data
+
+# data=[plot_channel(5)]
+
+
+# for i in range(1):
+# data.append(plot_channel(4))
+
+
+pyplot.plot(plot_channel(0))
+pyplot.plot(plot_channel(1))
+pyplot.plot(plot_channel(2))
+pyplot.plot(plot_channel(4))
+pyplot.plot(plot_channel(4))
+pyplot.plot(plot_channel(5))
+pyplot.plot(plot_channel(6))
+pyplot.plot(plot_channel(7))
+
+pyplot.show()
